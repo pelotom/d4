26d28054ae249f48a90a28f72221ac0bbc0f4220
Tom Crockett <pelotom@gmail.com>
2013-11-11 14:47:15 -0800

    Unique CSS class assignment, allow partial data functions

--- d4.js
+++ d4.js
@@ -1,24 +1,20 @@
 var d4 = (function () {
 
-function noop(){}
+function identity(x) { return x; }
 
 function defaultSpec(type) {
   return {
-    update: noop,
-    merge: noop,
-    enter: noop,
-    exit: noop,
+    update: identity,
+    merge: identity,
+    enter: identity,
+    exit: identity,
     children: []
   };
 }
 
-var idCounter = 0;
-
-function Figure(parent, spec) {
+function Figure(parent, spec, figureId) {
 
   var self = this;
-
-  var figureId = 'figure-' + (idCounter++);
 
   if (_.isFunction(spec))
     spec = spec();
@@ -26,24 +22,35 @@ function Figure(parent, spec) {
 
   function draw(data) {
     // If no data specified, use the same data as the parent
-    data = data || function(data) { return [data]; };
+    if (_.isFunction(data)) {
+      // allow data functions to be partial, treating undefined as []
+      var f = data;
+      data = function(d) {
+        if (_.isUndefined(d = f(d)))
+          return [];
+        return d;
+      };
+    } else
+      data = data || function(data) { return [data]; };
 
     var sel = parent.selectAll(spec.type + '.' + figureId).data(data, spec.key);
 
     function doPhase(phase, sel) {
-      return sel.each(function(d, i) {
-        spec[phase].call(self, d3.select(this), d, i);
+      var resultSels = [];
+      sel.each(function(d, i) {
+        resultSels.push(spec[phase].call(self, d3.select(this), d, i));
       });
+      return resultSels;
     }
 
     doPhase('update', sel);
     doPhase('enter', sel.enter().append(spec.type).classed(figureId, true));
     doPhase('merge', sel);
-    doPhase('exit', sel.exit()).remove();
+    doPhase('exit', sel.exit()).forEach(function (sel) { sel.remove(); });
 
     if (!sel.empty()) {
-      spec.children.forEach(function (childGroup) {
-        d4.build(sel, childGroup.spec).draw(childGroup.data);
+      spec.children.forEach(function (childGroup, childIndex) {
+        new Figure(sel, childGroup.spec, figureId + '-' + childIndex).draw(childGroup.data);
       });
     }
   };
@@ -52,7 +59,7 @@ function Figure(parent, spec) {
 }
 
 return {
-  build: function(parent, spec) { return new Figure(parent, spec); }
+  build: function(parent, spec) { return new Figure(parent, spec, 'figure'); }
 };
 
 })();
