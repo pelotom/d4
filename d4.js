28676d90427551fd2cb7cb63e8f7c50ce3d82048
Tom Crockett <pelotom@gmail.com>
2013-11-10 20:44:04 -0800

    Basic phases implemented

--- d4.js
+++ d4.js
@@ -1,59 +1,50 @@
 var d4 = (function () {
 
-function copy(obj) {
-  var clone = {};
-  for (var p in obj)
-    clone[p] = obj[p];
-  return clone;
-}
+function noop(){}
 
-function Figure(fields) {
-
-  var self = this;
-
-  function addBuilder(field, multiArg, append) {
-    if (arguments.length < 2)
-      multiArg = false;
-    if (arguments.length < 3)
-      append = false;
-
-    self[field] = function(value) {
-      if (arguments.length === 0)
-        return fields[field];
-
-      var newFields = copy(fields);
-      if (value === undefined)
-        delete newFields[field];
-      else {
-        var newVal = multiArg ? Array.prototype.slice.call(arguments) : value;
-        if (append) {
-          if (!(field in newFields))
-            newFields[field] = [];
-          newFields[field].push(newVal);
-        } else
-          newFields[field] = newVal;
-      }
-
-      return new Figure(newFields);
-    };
-  }
-
-  // Add chainable setters for simple fields
-  addBuilder('id');
-  addBuilder('enter');
-  addBuilder('update');
-  addBuilder('merge');
-  addBuilder('exit');
-  addBuilder('children', true, true);
-
-  self.fields = function() {
-    return copy(fields);
+function defaultSpec(type) {
+  return {
+    update: noop,
+    merge: noop,
+    enter: noop,
+    exit: noop,
+    children: []
   };
 }
 
+var idCounter = 0;
+
+function Figure(spec) {
+
+  var figureId = 'figure-' + (idCounter++);
+
+  spec = _.extend({}, defaultSpec(spec.type), spec);
+
+  function draw(parent, data) {
+    var sel = parent.selectAll(spec.type + '.' + figureId).data(data, spec.key);
+
+    function doPhase(phase, sel) {
+      return sel.each(function(d, i) {
+        spec[phase](d3.select(this), d, i);
+      });
+    }
+
+    doPhase('update', sel);
+    var enter = sel.enter().append(spec.type).classed(figureId, true);
+    doPhase('enter', enter);
+    doPhase('merge', sel);
+    doPhase('exit', sel.exit()).remove();
+
+    spec.children.forEach(function (childGroup) {
+      d4.build(childGroup.spec).draw(sel, childGroup.data);
+    });
+  };
+
+  this.draw = draw;
+}
 
 return {
-  figure: function(elemType) { return new Figure({'elemType': elemType}); }
+  build: function(spec) { return new Figure(spec); }
 };
 
 })();
