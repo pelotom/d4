a9c6a2816ceccc5dd739824b5a597c4ab2f39749
Tom Crockett <pelotom@gmail.com>
2013-11-16 15:04:14 -0800

    Move workhorse render function out of Spec class

--- d4.js
+++ d4.js
@@ -9,6 +9,8 @@
     }
 }(this, function (d3) {
 
+var d4; // the module being defined
+
 function identity(x) { return x; }
 
 function constant(x) { return function() { return x; } }
@@ -34,106 +36,119 @@ function Spec(elemType, fields) {
   } else
     addFields = false;
 
-  function addBuilder(field, def) {
+  function lastSetting(settings) {
+    return settings.length === 0 ? undefined : settings[settings.length - 1];
+  }
+
+  function addBuilder(field, def, accessor) {
     if (addFields) {
       fields[field] = [];
-      if (arguments.length > 1)
+      if (!isUndefined(def))
         fields[field].push([def]); 
     }
+    if (isUndefined(accessor))
+      accessor = function(settings) {
+        var args = lastSetting(settings);
+        return isUndefined(args) ? undefined : args[0];
+      };
     self[field] = function() {
+      if (arguments.length === 0) // invoked as an accessor
+        return accessor(fields[field]);
       var newFields = shallowCopy(fields);
       newFields[field] = newFields[field].concat([Array.prototype.slice.call(arguments)]);
       return new Spec(elemType, newFields);
     };
   }
 
-  function lastSetting(field) {
-    var settings = fields[field];
-    return settings.length === 0 ? undefined : settings[settings.length - 1][0];
-  }
-
   // Add chainable setters for simple fields
+  addBuilder('elemType', elemType);
   addBuilder('key');
   addBuilder('enter', identity);
   addBuilder('update', identity);
   addBuilder('merge', identity);
   addBuilder('exit', identity);
-  addBuilder('children');
+  addBuilder('children', undefined, identity);
 
   // A convenience method for building a singleton child group
   self.child = function(childSpec, childData) {
     return self.children(childSpec, childData, true);
   }
-
-  function render(sel, deep, renderId) {
-
-    function doPhase(phase, sel) {
-      var resultSels = [];
-      sel.each(function(d, i) {
-        resultSels.push(lastSetting(phase).call(self, d3.select(this), d, i));
-      });
-      return resultSels;
-    }
-
-    doPhase('update', sel);
-    
-    var enter = sel.enter().append(elemType)
-      .classed(renderId, true)
-      .each(function() {
-        this['__d4_data__'] = {
-          spec: self,
-          renderId: renderId
-        };
-      })
-      ;
-    doPhase('enter', enter);
-
-    doPhase('merge', sel);
-
-    var exit = doPhase('exit', sel.exit());
-    exit.forEach(function (sel) { sel.remove(); });
-
-    if (deep && !sel.empty())
-      renderChildren(sel, renderId);
-  }
-
-  function renderChildren(sel, renderId) {
-    fields['children'].forEach(function (childGroup, childIndex) {
-      var childSpec = childGroup[0];
-      var childData = childGroup[1];
-      var singleton = childGroup.length > 2 ? childGroup[2] : false;
-
-      if (isFunction(childData)) {
-        var f = childData;
-        childData = function(d) {
-          if (isUndefined(d = f(d)))
-            // Allow data functions to be partial, treating undefined as []
-            return [];
-          return singleton ? [d] : d;
-        };
-      } else if (isUndefined(childData)) {
-        // No data specified -- inherit data from the parent
-        childData = function(data) { return singleton ? [data] : data; };
-      }
-      
-      if (isFunction(childSpec))
-        // Force lazy specs here
-        childSpec = childSpec();
-
-      childSpec.render(sel, childData, renderId + '-' + childIndex)
-    });
-  }
-
-  this._render = render;
-
-  this.render = function(parent, data, renderId) {
-    renderId = renderId || 'd4';
-    var sel = parent.selectAll('.' + renderId).data(data, lastSetting('key'));
-    render(sel, true, renderId);
-    return sel;
-  };
 }
 
+function render(spec, sel, deep, renderId) {
+
+  function doPhase(phase, sel) {
+    var resultSels = [];
+    sel.each(function(d, i) {
+      resultSels.push(spec[phase]().call(spec, d3.select(this), d, i));
+    });
+    return resultSels;
+  }
+
+  doPhase('update', sel);
+  
+  var enter = sel.enter().append(spec.elemType())
+    .classed(renderId, true)
+    .each(function() {
+      this['__d4_data__'] = {
+        spec: spec,
+        renderId: renderId
+      };
+    })
+    ;
+  doPhase('enter', enter);
+
+  doPhase('merge', sel);
+
+  var exit = doPhase('exit', sel.exit());
+  exit.forEach(function (sel) { sel.remove(); });
+
+  if (deep && !sel.empty())
+    renderChildren(spec, sel, renderId);
+}
+
+function renderChildren(spec, sel, renderId) {
+  return spec.children().map(function (childGroup, childIndex) {
+    var childSpec = childGroup[0];
+    var childData = childGroup[1];
+    var singleton = childGroup.length > 2 ? childGroup[2] : false;
+
+    if (isFunction(childData)) {
+      var f = childData;
+      childData = function(d) {
+        if (isUndefined(d = f(d)))
+          // Allow data functions to be partial, treating undefined as []
+          return [];
+        return singleton ? [d] : d;
+      };
+    } else if (isUndefined(childData)) {
+      // No data specified -- inherit data from the parent
+      childData = function(data) { return singleton ? [data] : data; };
+    }
+
+    return d4.render(childSpec, sel, childData, renderId + '-' + childIndex);
+  });
+}
+
+// The module itself is a function for building specs
+d4 = function (elemType) {
+  return new Spec(elemType);
+};
+
+d4.render = function(spec, parentSel, data, renderId) {
+  renderId = renderId || 'd4';
+
+  // Force lazy specs
+  if (isFunction(spec))
+    spec = spec();
+
+  var sel = parentSel.selectAll('.' + renderId).data(data, spec.key());
+  render(spec, sel, true, renderId);
+  return sel;
+};
+
+// Add a render method to d3 selections that allows them to re-render themselves
+// based on a previously-assigned spec
 d3.selection.prototype.render = function(deep) {
   deep = isUndefined(deep) ? true : deep;
 
@@ -144,13 +159,11 @@ d3.selection.prototype.render = function(deep) {
 
   var d4Data = sel.node()['__d4_data__'];
 
-  d4Data.spec._render(sel, deep, d4Data.renderId);
+  render(d4Data.spec, sel, deep, d4Data.renderId);
 
   return this;
 };
 
-return function(elemType) {
-  return new Spec(elemType);
-};
+return d4;
 
 }));
